31af19cb0d94a97b40727c20f350856b7d1ad505
MumtahenaH <manty905@gmail.com>
2025-01-05 13:41:34 +0600

    base created

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import DP from './DP.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={DP} className="DP-logo" alt="logo" />
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
